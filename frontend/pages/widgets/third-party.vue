9273389b3cdbe39952f6968f0e7e9948b88c7e59
nampth <nampth@kalapa.vn>
2020-09-22 14:29:31 +0700

    first commit

---
+++ frontend/pages/widgets/third-party.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <script>
+            window.fbAsyncInit = function () {
+                FB.init({
+                    appId: '266546391059558',
+                    cookie: true,
+                    xfbml: true,
+                    version: 'v8.0'
+                });
+
+                FB.AppEvents.logPageView();
+
+            };
+
+            (function (d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {
+                    return;
+                }
+                js = d.createElement(s);
+                js.id = id;
+                js.src = "https://connect.facebook.net/vi_vn/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ThirdPartyJs"
+    }
+</script>
